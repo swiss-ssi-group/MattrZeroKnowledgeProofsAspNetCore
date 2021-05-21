e4b0e1d00c5e8d5656b7dcf5055af3b9668b8c56
damienbod <damien_bod@hotmail.com>
2021-05-21 21:46:39 +0200

    code clean up UI

--- src/VaccineVerify/Controllers/VerificationController.cs
+++ src/VaccineVerify/Controllers/VerificationController.cs
@@ -24,50 +24,51 @@ namespace VaccineVerify.Controllers
         /// {
         ///  "presentationType": "QueryByExample",
         ///  "challengeId": "GW8FGpP6jhFrl37yQZIM6w",
-        ///  "claims": {
-        ///      "id": "did:key:z6MkfxQU7dy8eKxyHpG267FV23agZQu9zmokd8BprepfHALi",
-        ///      "name": "Chris",
-        ///      "firstName": "Shin",
-        ///      "licenseType": "Certificate Name",
-        ///      "dateOfBirth": "some data",
-        ///      "licenseIssuedAt": "dda"
-        ///  },
         ///  "verified": true,
         ///  "holder": "did:key:z6MkgmEkNM32vyFeMXcQA7AfQDznu47qHCZpy2AYH2Dtdu1d"
         /// }
         /// </summary>
         /// <param name="body"></param>
         /// <returns></returns>
+        //[HttpPost]
+        //[Route("[action]")]
+        //public async Task<IActionResult> VerificationDataCallback([FromBody] VerifiedVaccinationData body)
+        //{
+        //    string connectionId;
+        //    var found = MattrVerifiedSuccessHub.Challenges
+        //        .TryGetValue(body.ChallengeId, out connectionId);
+
+        //    // test Signalr
+        //    //await _hubContext.Clients.Client(connectionId).SendAsync("MattrCallbackSuccess", $"{body.ChallengeId}");
+        //    //return Ok();
+
+        //    var exists = await _VaccineVerifyDbService.ChallengeExists(body.ChallengeId);
+
+        //    if (exists)
+        //    {
+        //        await _VaccineVerifyDbService.PersistVerification(body);
+
+        //        if (found)
+        //        {
+        //            //$"/VerifiedUser?challengeid={body.ChallengeId}"
+        //            await _hubContext.Clients
+        //                .Client(connectionId)
+        //                .SendAsync("MattrCallbackSuccess", $"{body.ChallengeId}");
+        //        }
+
+        //        return Ok();
+        //    }
+
+        //    return BadRequest("unknown verify request");
+        //}
+
         [HttpPost]
         [Route("[action]")]
-        public async Task<IActionResult> VerificationDataCallback([FromBody] VerifiedVaccinationData body)
+        public async Task<IActionResult> VerificationDataCallback([FromBody] object body)
         {
-            string connectionId;
-            var found = MattrVerifiedSuccessHub.Challenges
-                .TryGetValue(body.ChallengeId, out connectionId);
+            var test = body.ToString();
+            return Ok();
 
-            // test Signalr
-            //await _hubContext.Clients.Client(connectionId).SendAsync("MattrCallbackSuccess", $"{body.ChallengeId}");
-            //return Ok();
-
-            var exists = await _VaccineVerifyDbService.ChallengeExists(body.ChallengeId);
-
-            if (exists)
-            {
-                await _VaccineVerifyDbService.PersistVerification(body);
-
-                if (found)
-                {
-                    //$"/VerifiedUser?challengeid={body.ChallengeId}"
-                    await _hubContext.Clients
-                        .Client(connectionId)
-                        .SendAsync("MattrCallbackSuccess", $"{body.ChallengeId}");
-                }
-
-                return Ok();
-            }
-
-            return BadRequest("unknown verify request");
         }
     }
 }
