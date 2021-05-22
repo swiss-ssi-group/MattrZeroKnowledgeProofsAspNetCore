13e2a60d1be1745663aeba7843b6bf712ef78889
damienbod <damien_bod@hotmail.com>
2021-05-22 10:22:45 +0200

    migrations

---
+++ src/VaccineVerify/Migrations/20210522082128_add-dids-id.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VaccineVerify.Migrations
+{
+    public partial class adddidsid : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Dids",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DidId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DidTypeId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DidData = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dids", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Dids");
+        }
+    }
+}
